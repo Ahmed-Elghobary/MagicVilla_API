956779d5ffd7f1e55d1397d893224d462f3013f5
Ahmed_Osama <ahmed.elghobary01@gmail.com>
2024-04-06 00:39:06 +0200

    Add project files.

---
+++ MagicVilla_VillaAPI/Steps.cs
@@ -0,0 +1,15 @@
+﻿namespace MagicVilla_VillaAPI
+{
+    public class Steps
+    {
+        /* 
+         * 
+        1- create project
+        2- Add source control
+
+         
+         
+         
+         */
+    }
+}
