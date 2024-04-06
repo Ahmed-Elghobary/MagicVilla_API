2e82aacdb9226e2d0fcf9a590de2e3e017263cfe
Ahmed_Osama <ahmed.elghobary01@gmail.com>
2024-04-06 07:03:21 +0200

    Add Vill Controller and Get EndPoint

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,23 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaAPIController:ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return VillaStore.VillaList;
+        }
+        [HttpGet("{id:int}")]
+        public VillaDto GetVilla(int id)
+        {
+            return VillaStore.VillaList.FirstOrDefault(u=>u.Id==id);
+        }
+    }
+}
